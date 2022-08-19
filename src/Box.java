87d4c60a16031c2cdfe3a2cead7315537293de8a
Рыскелди <ryskeldi.osmonaliev@mail.ru>
2022-08-19 20:01:27 +0300

    commit

--- src/Box.java
+++ src/Box.java
@@ -1,9 +1,27 @@
 public class Box<T> {
+    @Override
+    public String toString() {
+        return "Box{" +
+                "t=" + t +
+                '}';
+    }
 
+    public Box(T t) {
+        this.t = t;
+    }
 
+    private T t;
+
+    public T getT() {
+        return t;
+    }
+
+    public void setT(T t) {
+        this.t = t;
+    }
 
     public static <T> T generic(T t){
-        System.out.println(t);
+
         return t;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-        ArrayList<String>list1 = new ArrayList<>();
-        list1.add("Nortan");
-        list1.add("Bobi");
-        list1.add("robert");
-        Box.generic(list);
-        Box.generic(list1);
+        Box<Integer> list1 = new Box<>(47);
+        System.out.println(Box.generic(list1));
     }
 }
